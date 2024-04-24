bdbaf5ceae50465b32cfcd6cf0f6b760e4b2960b
adnanrony <adnanrony19@gmail.com>
2024-04-24 21:26:14 +0600

    data tuype

---
+++ second-Program/index.js
@@ -0,0 +1,13 @@
+var userid;
+var firstname;
+var lastname;
+var fulname;
+var isgood;
+userid = 5155;
+firstname = "adnan ";
+lastname = "rony";
+isgood = true;
+fulname = firstname.concat(lastname);
+console.log("Your id : ".concat(userid, ",username:").concat(fulname, " isgood: ").concat(isgood));
+// split
+console.log(fulname.split(" "));
---
+++ second-Program/index.ts
@@ -0,0 +1,21 @@
+
+
+let userid:number;
+let firstname:string;
+let lastname:string;
+let fulname:string;
+let isgood:boolean
+
+userid=5155;
+firstname="adnan ";
+lastname="rony";
+isgood=true
+
+fulname=firstname.concat(lastname)
+
+
+
+console.log(`Your id : ${userid},username:${fulname} isgood: ${isgood}`)
+
+// split
+console.log(fulname.split(" "))
